d1958a3965f5ac3b06d169968d423b33d8d4a9a7
mightyteja <jaisreram2003@gmail.com>
2018-03-18 11:27:48 +0530

    home page completed

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>import all the content</title>
+<meta http-equiv="description" content="page description" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<style type="text/css">@import "footer/footer.css";</style>
+<style type="text/css">@import "header/header.css";</style>
+<style type="text/css">@import "home/home.css";</style>
+<style type="text/css">@import "styles.css";</style>
+
+</head>
+
+
+<?php include("header/header.html");?>
+<body>
+<?php include("home/home.html");?>
+</body>
+<?php include("footer/footer.html");?>
+
+
+</html>
